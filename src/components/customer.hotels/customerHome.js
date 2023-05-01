cbf79dc27903adf11c1ce66384a8494010ea6db5
kalanarathnayake <kalanagayanga8@gmail.com>
2023-05-01 23:35:59 +0530

    feat: homepage

--- src/components/customer.hotels/customerHome.js
+++ src/components/customer.hotels/customerHome.js
@@ -12,7 +12,7 @@ class customerHome extends Component {
           </div>
         </section>
 
-        <div class=" container mx-auto px-5 py-2 lg:px-32 lg:pt-12 mb-5 grid grid-cols-3 content-center">
+        <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 mb-5 grid grid-cols-3 content-center">
           <div className="shadow-xl card w-96 bg-base-100">
             <figure><img src="https://wallpapershome.com/images/pages/pic_h/16232.jpg" alt="Shoes" /></figure>
             <div className="card-body">
@@ -44,7 +44,96 @@ class customerHome extends Component {
             </div>
           </div>
         </div>
-        
+        {/* events and meetings */}
+        <section class="">
+          <div class="text-center bg-white text-gray-800 px-6">
+            <h1 class="text-5xl text-blue-950 md:text-6xl xl:text-3xl font-bold tracking-tight uppercase drop-shadow-md ">Events and Meetings<br />
+            </h1>
+          </div>
+        </section>
+
+        <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 " >
+          <div class="p-10">
+            <a href="/" class=" flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-7xl h-96 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img src="https://wallpapershome.com/images/pages/pic_h/336.jpg" alt="" class=" grow object-cover rounded-t-lg h-96  md:w-48 md:rounded-none md:rounded-l-lg" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+
+          <div class="p-10">
+            <a href="/" class=" flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-7xl h-96 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img src="https://wallpapershome.com/images/pages/pic_h/336.jpg" alt="" class=" grow object-cover rounded-t-lg h-96  md:w-48 md:rounded-none md:rounded-l-lg" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        {/* Wedding Halls */}
+        <section class="">
+          <div class="text-center bg-white text-gray-800 px-6">
+            <h1 class="text-5xl md:text-6xl xl:text-3xl font-bold tracking-tight uppercase drop-shadow-md text-blue-950">Events and Meetings<br />
+            </h1>
+          </div>
+        </section>
+
+        <div class=" container mx-auto px-5 py-2 lg:px-32 lg:pt-12 " >
+          <div class="p-10">
+            <a href="/" class=" flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-7xl h-96 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img src="https://wallpapershome.com/images/pages/pic_h/336.jpg" alt="" class=" grow object-cover rounded-t-lg h-96  md:w-48 md:rounded-none md:rounded-l-lg" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+
+          <div class="p-10">
+            <a href="/" class=" flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-7xl h-96 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img src="https://wallpapershome.com/images/pages/pic_h/336.jpg" alt="" class=" grow object-cover rounded-t-lg h-96  md:w-48 md:rounded-none md:rounded-l-lg" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        {/* Accomodations*/}
+        <section class="">
+          <div class="text-center bg-white text-gray-800 px-6">
+            <h1 class="text-5xl md:text-6xl xl:text-3xl font-bold tracking-tight uppercase drop-shadow-md text-blue-950 ">Accomodations<br />
+            </h1>
+          </div>
+        </section>
+
+        <div class=" container mx-auto px-5 py-2 lg:px-32 lg:pt-12 " >
+          <div class="p-10">
+            <a href="/" class=" flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-7xl h-96 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img src="https://wallpapershome.com/images/pages/pic_h/336.jpg" alt="" class=" grow object-cover rounded-t-lg h-96  md:w-48 md:rounded-none md:rounded-l-lg" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+
+          <div class="p-10">
+            <a href="/" class=" flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-7xl h-96 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img src="https://wallpapershome.com/images/pages/pic_h/336.jpg" alt="" class=" grow object-cover rounded-t-lg h-96  md:w-48 md:rounded-none md:rounded-l-lg" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+{/*  */}
         <div class="mb-24 container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
           <div class="-m-1 flex flex-wrap md:-m-2">
             <div class="flex w-1/3 flex-wrap">
