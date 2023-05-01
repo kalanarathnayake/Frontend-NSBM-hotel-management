129f9f690a1c7ba60be79811ee78442be017d8b9
kalanarathnayake <kalanagayanga8@gmail.com>
2023-05-01 18:48:22 +0530

    Feat: homepage UI updated

--- src/components/navbar/home.js
+++ src/components/navbar/home.js
@@ -6,12 +6,46 @@ class home extends Component {
       <div class="">
         <section class="">
           <div class="text-center bg-white text-gray-800 py-20 px-6">
-            <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight uppercase mb-8 drop-shadow-md ">delicious and freshly<br />
-              <span class="text-orange-600 ">baked </span>
-              <span class="text-orange-500 animate-pulse "> PizzaMania </span>
+            <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight uppercase mb-8 drop-shadow-md ">HOTELS IN SRI LANKA<br />
+              <span class="text-blue-950 font-serif font-light  ">JETWING HOTELS </span>
+              {/* <span class="text-orange-500 animate-pulse "> PizzaMania </span> */}
             </h1>
           </div>
         </section>
+
+        <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 mb-5 grid grid-cols-3 content-center">
+          <div className="shadow-xl card w-96 bg-base-100">
+            <figure><img src="https://wallpapershome.com/images/pages/pic_h/16232.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="justify-center card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="justify-center card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-xl card w-96 bg-base-100">
+            <figure><img src="https://wallpapershome.com/images/pages/pic_h/6498.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="justify-center card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="justify-center card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-xl card w-96 bg-base-100">
+            <figure><img src="https://wallpapershome.com/images/pages/pic_h/336.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="justify-center card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="justify-center card-actions">
+                <button className="btn btn-primary ">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
         <div class="mb-24 container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
           <div class="-m-1 flex flex-wrap md:-m-2">
             <div class="flex w-1/3 flex-wrap">
@@ -19,7 +53,7 @@ class home extends Component {
                 <img
                   alt="gallery"
                   class="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8zJX9p57ngDAHaUwD8Hs_1PXoRe3k0KVsmQ&usqp=CAU"
+                  src="https://www.jetwinghotels.com/wp-content/uploads/2019/06/colombo-hotel-category-744x653.jpg"
                 />
               </div>
             </div>
@@ -28,7 +62,7 @@ class home extends Component {
                 <img
                   alt="gallery"
                   class="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://img.freepik.com/free-photo/mixed-pizza-with-various-ingridients_140725-3790.jpg?w=2000"
+                  src="https://www.jetwinghotels.com/wp-content/uploads/2018/07/mermaid-hotel-kalutara.jpg"
                 />
               </div>
             </div>
@@ -37,7 +71,7 @@ class home extends Component {
                 <img
                   alt="gallery"
                   class="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://www.kingarthurbaking.com/sites/default/files/2022-03/Easiest-Pizza_22-2_11.jpg" />
+                  src="https://www.jetwinghotels.com/wp-content/uploads/2017/10/Colombo-7-categorythumbnail.jpg" />
               </div>
             </div>
             <div class="flex w-1/3 flex-wrap">
@@ -45,7 +79,7 @@ class home extends Component {
                 <img
                   alt="gallery"
                   class="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-4Lb7iNiT2WSAqvGHO3NowsQGRCrMVcGEUQ&usqp=CAU"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQceBMogAcpWxMh3HK4QGYZrGPKlIwN-Tximyd0rj8c-4sDIWZ7EGdrKe_G6jaFPIE_iSs&usqp=CAU"
                 />
               </div>
             </div>
@@ -54,7 +88,7 @@ class home extends Component {
                 <img
                   alt="gallery"
                   class="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://static.toiimg.com/thumb/msid-87930581,imgsize-211826,width-800,height-600,resizemode-75/87930581.jpg" />
+                  src="https://www.jetwinghotels.com/wp-content/uploads/2019/06/bentota-hotels-category-744x653-1.jpg" />
               </div>
             </div>
             <div class="flex w-1/3 flex-wrap">
@@ -62,7 +96,7 @@ class home extends Component {
                 <img
                   alt="gallery"
                   class="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTITXxQQ4cSLkQUGaG-qaxSq1ioDHTtwFYmWA&usqp=CAU" />
+                  src="https://www.jetwinghotels.com/jetwinglake/wp-content/uploads/sites/8/2017/11/lake-Gallery2.jpg" />
               </div>
             </div>
           </div>
